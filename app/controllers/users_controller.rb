f83cfbac994e57a0d3ba08a1fb1e5bc62ce2d4a0
gerald hernandez <gerald2557@gmail.com>
2016-05-17 23:55:12 -0700

    got user signups working, might need to fix flash messages taking up too much space after signing up, note to self: I left off at chapter 8 of the rails tutorial for just modeling my user https://www.railstutorial.org/book/log_in_log_out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,30 @@
 class UsersController < ApplicationController
+  # https://www.railstutorial.org/book/modeling_users 6.10
   def new
+    @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome, account created! Have fun storing some notes."
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+    def user_params
+    params.require(:user).permit(:email, :name, :password, :password_confirmation)
+    end
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ActiveRecord::Base
-    # https://www.railstutorial.org/book/modeling_users 6.10
     before_save {self.email = email.downcase}
     validates :name, presence: true, length: { maximum: 50 }
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 },
                       format: {with: VALID_EMAIL_REGEX},
                       uniqueness: {case_sensitive: false}
